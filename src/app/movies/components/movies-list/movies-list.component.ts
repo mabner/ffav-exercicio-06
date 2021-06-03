afd62034a1adfc52e7cece8235256ae36f1a6a06
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-03 18:23:39 -0300

    Optimize image size for speed

--- src/app/movies/components/movies-list/movies-list.component.ts
+++ src/app/movies/components/movies-list/movies-list.component.ts
@@ -8,7 +8,7 @@ import { MoviesService } from '../../services/movies.service';
   styleUrls: ['./movies-list.component.css'],
 })
 export class MoviesListComponent implements OnInit {
-  private BASE_URL_IMAGE = `https://image.tmdb.org/t/p/original`;
+  private BASE_URL_IMAGE = `https://image.tmdb.org/t/p/w200`;
   @Input() movies: Array<IMovieItem> = [];
   constructor(private moviesService: MoviesService) {}
 
