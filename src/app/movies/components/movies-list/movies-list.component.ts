877a7e69878e755e3867f2956e03871ab923087b
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-04 02:35:57 -0300

    Search back to normal, detail still now working

--- src/app/movies/components/movies-list/movies-list.component.ts
+++ src/app/movies/components/movies-list/movies-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IMovieItem } from '../../models/IMovieItem';
 import { MoviesService } from '../../services/movies.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movies-list',
@@ -8,15 +9,22 @@ import { MoviesService } from '../../services/movies.service';
   styleUrls: ['./movies-list.component.css'],
 })
 export class MoviesListComponent implements OnInit {
-  @Input() id: Number;
+  ID: Number;
   BASE_URL_IMAGE = `https://image.tmdb.org/t/p/w200`;
   @Input() movies: Array<IMovieItem> = [];
 
-  constructor(private moviesService: MoviesService) {}
+  constructor(
+    private moviesService: MoviesService,
+
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.moviesService.getMovies().subscribe((data) => {
       this.movies = data.results;
     });
+    this.route.params.subscribe((params) => {
+      this.ID = params['id'];
+    } );
   }
 }
